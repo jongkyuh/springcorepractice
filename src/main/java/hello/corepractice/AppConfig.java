623ce7002ebdcc6e52fc6521201527c66dde4587
jongkyuh <jongkyuh@naver.com>
2024-06-08 15:59:26 +0900

    4. AppConfig에서 역할에 따른 구현을 잘 알수없어 리팩토링 진행, 변경 된 방식으로 고정할인 정책에서 정률할인 정책으로 변경
    기존에 구현클래스에서 new로 의존클래스를 만들어서 DIP와 OCP 둘다 지키지 못했기 때문에 AppConfig 클래스를 만들어 해당 클래스에서 의존관계 주입이 필요한 클래스에 직접 의존관계를 넣어준다.DIP도 지키지 못하는 예제가 됩니다. 이후에는 둘 다 적용할 수
       있는 로직으로 변경하며 학습할 예정
    ->
    AppConfig에서 역할에 따른 구현을 제대로 알 수 없어 구현체를 만들 때 new로 중복하며 만든 부분을 메소드로 만들어 중복을 피했고, 또한 구현체의 역할을 정확히 볼 수 있게 되었습니다.
    그리고 다른 할인정책을 도입할 때 클라이언트 코드를 수정하는 것이 아닌 AppConfig에서 의존관계만 변경해 적용할 수 있다.

--- src/main/java/hello/corepractice/AppConfig.java
+++ src/main/java/hello/corepractice/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.corepractice;
 
+import hello.corepractice.discount.DiscountPolicy;
 import hello.corepractice.discount.FixDiscountPolicy;
+import hello.corepractice.discount.RateDiscountPolicy;
 import hello.corepractice.member.MemberService;
 import hello.corepractice.member.MemberServiceImpl;
 import hello.corepractice.member.MemoryMemberRepository;
@@ -9,13 +11,39 @@ import hello.corepractice.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    // 기존 코드에서는 AppConfig 클래스의 코드만을 보고 어떤역할을 하는지에 대한 것을 보기가 어렵다 그래서 분리해야한다. 
+    
     // 생성자 주입
+    
+//    //기존 코드 
+//    public MemberService memberService(){        
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+//    }
+    
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     // 생성자 주입
+
+    //기존코드
+
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
+    }
+
+    //FixDiscountPolicy은 자동완성을 사용하지않고 적용
+//    public DiscountPolicy discountPolicy(){ // 고정할인정책
+//        return new FixDiscountPolicy();
+//    }
+
+
+    // 정률할인 정책으로 변경
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
     }
 }
