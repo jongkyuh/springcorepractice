75bd9169a8b26b722f4472924bc8157e07652f68
jongkyuh <jongkyuh@naver.com>
2024-06-19 21:27:54 +0900

    9. @Configuration 애노테이션의 원리
    @Configuration 애노테이션을 사용하면 스프링 컨테이너에 AppConfig 클래스가 등록되는 것이 아닌 바이트 코드가 조작된 AppConfig클래스를 상속받은 임의의 클래스가 등록된다. 그래서 싱글톤을 유지할 수 있고, 만약 애노테이션이 없다면 빈 등록은 되지만 memberRepository는 3번 호출되고 각각 다른 인스턴스가 주입된다.

--- src/test/java/hello/corepractice/singleton/ConfigurationSingleTest.java
+++ src/test/java/hello/corepractice/singleton/ConfigurationSingleTest.java
@@ -32,4 +32,19 @@ public class ConfigurationSingleTest {
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void confiugurationDeep(){
+        
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean.getClass() = " + bean.getClass());
+
+        // 출력결과가 bean.getClass() = class hello.corepractice.AppConfig가 아니라
+        // bean.getClass() = class hello.corepractice.AppConfig$$SpringCGLIB$$0 로 나오게 된다.
+        // 순수한 자바 클래스인 AppCofig 클래스를 싱글톤으로 관리하기 위해 AppConfig를 상속받은 임의의 클래스를 만들어
+        // 이 클래스를 스프링에 등록한 것이다. 그래서 부모타입인 AppConfig.class로 조회가 가능하다.
+    }
 }
