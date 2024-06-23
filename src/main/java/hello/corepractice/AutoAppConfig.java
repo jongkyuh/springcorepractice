2170fbe6fd6989ced7e8b8369473591943c31679
jongkyuh <jongkyuh@naver.com>
2024-06-23 21:57:24 +0900

    11. ComponentScan 옵션
    basePackages = "패키지위치"
    -> 지정한 패키지 하위 모두 스캔
    
    basePackageClasses = aaa.class
    -> 지정한 클래스가 속해있는 패키지 모두 스캔

--- src/main/java/hello/corepractice/AutoAppConfig.java
+++ src/main/java/hello/corepractice/AutoAppConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan( // 기존 AppConfig와 같은 예제코드를 중복으로 스프링빈으로 등록하지 않기 위해서 제외시켜준다. 그래야 나중에
-                    // 복습할 때에도 실행 가능하기 위해서 
+                    // 복습할 때에도 실행 가능하기 위해서
+    //    basePackages = "hello.corepractice",
+    //    basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
