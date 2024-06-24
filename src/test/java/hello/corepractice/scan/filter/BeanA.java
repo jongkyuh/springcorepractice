e38b4a3f5d67c57a69dd3b33457bea113afbc579
jongkyuh <jongkyuh@naver.com>
2024-06-24 23:53:08 +0900

    12. ComponentScan 필터
    includeFilters와 excludeFilter를 통해 빈으로 등록하고자 하는 클래스와 제외하고자 하는 클래스를 지정할 수 있다.

---
+++ src/test/java/hello/corepractice/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package hello.corepractice.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ src/test/java/hello/corepractice/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package hello.corepractice.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+}
