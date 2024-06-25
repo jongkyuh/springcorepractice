7c9b13173e36e91e357936a422f7353b93b68036
jongkyuh <jongkyuh@naver.com>
2024-06-25 21:10:03 +0900

    13. 빈 충돌
    자동 빈등록 vs 자동 빈 등록을 할때는 바로 에러가 뜨기떄문에 구분 할 수있는데
    수동 빈등록과 자동 빈 등록이 충돌 할 때는 수동빈 등록이 우선권을 가지기 때문에 테스트에서 통과하지만, 스프링부트를 실행하면 오류가 나게된다. 빈 오버라이딩 옵션이 false되어있기 떄문인데 이럴떈 해당 옵션을 true로 properties에서 바꿔준다. 참고로 스프링부트 3.2.x 버전부터는 ide에서는 해결하지 못하는데, 이럴땐 build를 gradle로 바꿔야 부트가 실행된다.

--- src/main/java/hello/corepractice/AutoAppConfig.java
+++ src/main/java/hello/corepractice/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.corepractice;
 
+import hello.corepractice.member.MemberRepository;
+import hello.corepractice.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,4 +15,13 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+//    @Bean("memoryMemberRepository")
+//        // 테스트를 통해서 빈등록을 하게되면 같은 이름이 있더라도 수동빈이 우선권을 갖게되어
+//        // 자동빈을 오버라이딩 해버린다.
+//        // 스프링 부트를 실행하게되면 오류가 난다.
+//
+//    MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
 }
